fbc7533f3c360b9b949cf44c8757fc850d0eca28
Kartik Kannapur <kartikkannapur@gmail.com>
2016-01-16 16:51:37 +0530

    Week 2 - PyMongo Queries

--- Week_2/02_pymongo_query_find.py
+++ Week_2/02_pymongo_query_find.py
@@ -20,4 +20,20 @@ collection = database.people
 
 
 # #Connect to the Database
+print " --- Return all documents --- "
+# #find() returns a cursor in python
 print list(collection.find())
+
+print " --- Find One document --- "
+print collection.find_one()
+
+print " --- Find One document with a query condition --- "
+query_1 = {"age" : 45}
+print collection.find_one(query_1)
+
+# #Projections
+print " --- Finding documents with Projections --- "
+query_2 = {"age" : 45}
+# #SELECT name & I don't want to see _id
+projection = {"name" : 1, "_id" : 0}
+print list(collection.find(query_2, projection))
